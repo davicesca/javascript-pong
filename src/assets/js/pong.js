77a9054586e136eec8ecfa64aaf7562344d7d046
davicesca <davifagundescesca@gmail.com>
2022-06-11 16:48:47 -0300

    Add game menu

--- src/assets/js/pong.js
+++ src/assets/js/pong.js
@@ -2,13 +2,14 @@
 let canvas;
 let canvasContext;
 
-// Game Variables 
-let gameMode;
+// Game Mode (0 = Menu; 1 = Game; 2 = Win Screen) 
+let gameMode = 0;
 
 /* CORE FUNCTIONS */
 export function load() {
     canvas = document.querySelector('.game-canvas');
     canvasContext = canvas.getContext('2d');
+    canvas.addEventListener('mousedown', handleMouseClick);
 }
 
 export function update() {
@@ -19,14 +20,27 @@ export function draw() {
     // Game Display
     colorRect(0, 0, canvas.width, canvas.height, 'black');
 
-    colorText('Oi! Tudo bem?', 100, 100, 'white');
+    if(gameMode === 0) { // Main Menu
+        const playTxt = 'CLICK ON SCREEN TO PLAY!';
+        colorText(playTxt, '32px', 
+        (canvas.width-canvasContext.measureText(playTxt).width)/2,
+        canvas.height/2, 
+        'white');
+        return;
+    }
+    else if(gameMode === 2) { // Win Screen
+        return;
+    }
+
+    // Game
 
 }
 
 /* GAME FUNCTIONS */
 
 /* Draws text on screen */
-function colorText(text, x, y, color) {
+function colorText(text, size, x, y, color) {
+    canvasContext.font= `${size} Arial`;
     canvasContext.fillStyle = color;
     canvasContext.fillText(text, x, y);
 }
@@ -43,4 +57,10 @@ function colorCircle(x, y, radius, color) {
     canvasContext.beginPath();
     canvasContext.arc(x, y, radius, 0, Math.PI*2, true);
     canvasContext.fill();
+}
+
+function handleMouseClick() {
+    if(gameMode === 0) {
+        gameMode = 1;
+    }
 }
