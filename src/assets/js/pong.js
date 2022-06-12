9aa5278ac1620918f6e01bc56eb4fcbb3cfd745b
davicesca <davifagundescesca@gmail.com>
2022-06-12 00:00:31 -0300

    Add basic ball movement

--- src/assets/js/pong.js
+++ src/assets/js/pong.js
@@ -5,6 +5,24 @@ let canvasContext;
 // Game Mode (0 = Menu; 1 = Game; 2 = Win Screen) 
 let gameMode = 0;
 
+// Paddle Variables
+const PADDLE_THICKNESS = 10;
+const PADDLE_HEIGHT = 100;
+
+// Player Variables
+let playerY = 250;
+let playerScore = 0;
+
+// Enemy Variables
+let enemyY = 250;
+let enemyScore = 0;
+
+// Ball Variables
+let ballX = 50;
+let ballSpeedX = 5;
+let ballY = 50;
+let ballSpeedY = 5;
+
 /* CORE FUNCTIONS */
 export function load() {
     canvas = document.querySelector('.game-canvas');
@@ -13,7 +31,10 @@ export function load() {
 }
 
 export function update() {
-
+    if(gameMode === 0 || gameMode === 2) {
+        return;
+    }
+    ballMovement();
 }
 
 export function draw() {
@@ -34,9 +55,58 @@ export function draw() {
 
     // Game
 
+    // Player Paddle
+    colorRect(PADDLE_THICKNESS, playerY, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white');
+
+    // AI/Enemy Paddle
+    colorRect(canvas.width - PADDLE_THICKNESS*2, enemyY, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white');
+    
+    // Ball
+    colorCircle(ballX, ballY, 10, 'white');
 }
 
-/* GAME FUNCTIONS */
+/* GAMEPLAY FUNCTIONS */
+
+/* All code related with ball movement  */
+function ballMovement() {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+    if(ballX > canvas.width){
+        // ballSpeedX *= -1;
+        if(sideCollision(enemyY)) {
+            playerScore++;
+        }
+    }
+    else if(ballX < 0) {
+        // ballSpeedX *= -1;
+        if(sideCollision(playerY)) {
+            enemyScore++;
+        }
+    }
+
+    if(ballY > canvas.height || ballY < 0) {
+        ballSpeedY *= -1;
+    }
+}
+
+/* Verifies if ball collided with sides */
+function sideCollision(paddleY) {
+    if(ballY >= paddleY && ballY <= paddleY+PADDLE_HEIGHT) {
+        ballSpeedX *= -1;
+        return false;
+    }
+    ballReset();
+    return true;
+}
+
+/* Reset ball's position to center */
+function ballReset() {
+    ballX = canvas.width/2;
+    ballY = canvas.height/2;
+}
+
+/* OTHER FUNCTIONS */
 
 /* Draws text on screen */
 function colorText(text, size, x, y, color) {
@@ -60,7 +130,7 @@ function colorCircle(x, y, radius, color) {
 }
 
 function handleMouseClick() {
-    if(gameMode === 0) {
+    if(gameMode === 0 || gameMode === 2) {
         gameMode = 1;
     }
 }
